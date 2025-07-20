36d816f23a725a025d3a2eff0a371bec06fce5b1
Viero19G <gabrielviero22@gmail.com>
2025-07-20 20:09:40 -0300

    creating new variables in Zombie structure

--- zombies-contract/src/zombie.rs
+++ zombies-contract/src/zombie.rs
@@ -4,6 +4,8 @@ multiversx_sc::derive_imports!();
 #[type_abi]
 #[derive(NestedEncode, NestedDecode, TopEncode, TopDecode)]
 pub struct Zombie<M: ManagedTypeApi> {
-    pub name: ManagedBuffer<M>,
-    pub dna: u64,
+  pub name: ManagedBuffer<M>,
+  pub dna: u64,
+  pub level: u16,
+  pub ready_time: u64,
 }
